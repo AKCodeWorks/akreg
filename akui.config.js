7250a174f70e63f1fc9e70bfa3a7b8dcadf5efc5
Adam Gillispie <95198544+agillispie@users.noreply.github.com>
2025-11-02 12:37:10 -0500

    update more bad paths and pkg json for public npm

--- akui.config.js
+++ akui.config.js
@@ -1,7 +1,19 @@
 const config = {
+	/**
+	 * Directory where components will be installed
+	 */
 	installDir: 'src/lib/components/ui',
+	/**
+	 * Directory where registry is located
+	 */
 	registryComponentDir: 'src/registry',
+	/**
+	 * Registry file location
+	 */
 	registryDir: 'src/registry/registry.json',
+	/**
+	 * URL to registry repository
+	 */
 	registryUrl: 'https://raw.githubusercontent.com/AKCodeWorks/akui/refs/heads/main'
 };
 
